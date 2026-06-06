5e451cc8f4e487639aad8adef8e7b101b7023497
Santiago Hernandez <shd03072007@gmail.com>
2026-06-05 23:28:32 -0400

    creacion de djikstra, actualizacion de otras clases para mejor implementacion y creacion de graf

--- src/proyecto_estructura_de_datos_sh_aq/Dijkstra.java
+++ src/proyecto_estructura_de_datos_sh_aq/Dijkstra.java
@@ -1,10 +1,159 @@
 package proyecto_estructura_de_datos_sh_aq;
 
 /**
- * Clase que implementa el algoritmo de Dijkstra para encontrar la ruta más corta.
- * 
+ * Algoritmo Dijkstra - Encuentra el camino más corto en un grafo ponderado.
  * @author santiagoh
  */
 public class Dijkstra {
     
+    /**
+     * Clase para guardar el resultado del algoritmo Dijkstra.
+     */
+    public static class ResultadoDijkstra {
+        public TablaHash<Integer, Double> distancias;
+        public TablaHash<Integer, Integer> anteriores;
+        
+        public ResultadoDijkstra(TablaHash<Integer, Double> distancias, TablaHash<Integer, Integer> anteriores) {
+            this.distancias = distancias;
+            this.anteriores = anteriores;
+        }
+    }
+    
+    /**
+     * Ejecuta el algoritmo Dijkstra desde un nodo origen.
+     * 
+     * @param grafo TablaHash donde clave es ID de neurona y valor es lista de sinapsis
+     * @param diccionarioNeuro TablaHash con diccionario de neurotransmisores
+     * @param idOrigen ID de la neurona origen
+     * @return ResultadoDijkstra con distancias y nodos anteriores
+     */
+    public static ResultadoDijkstra buscar(TablaHash<Integer, ListaEnlazada<Sinapsis>> grafo, 
+                                           TablaHash<String, Neurotransmisor> diccionarioNeuro, 
+                                           int idOrigen) {
+        TablaHash<Integer, Double> distancias = new TablaHash<>();
+        TablaHash<Integer, Integer> anteriores = new TablaHash<>();
+        TablaHash<Integer, Boolean> visitados = new TablaHash<>();
+        
+        Object[] claves = grafo.getClaves();
+        for (int i = 0; i < claves.length; i++) {
+            Integer id = (Integer) claves[i];
+            distancias.insertar(id, Double.MAX_VALUE);
+        }
+        
+        distancias.insertar(idOrigen, 0.0);
+        
+        while (hayNoVisitados(distancias, visitados)) {
+            int nodoActual = obtenerNodoNoVisitadoConMenorDistancia(distancias, visitados);
+            
+            if (nodoActual == -1) {
+                break;
+            }
+            
+            visitados.insertar(nodoActual, true);
+            
+            ListaEnlazada<Sinapsis> sinapsis = grafo.obtener(nodoActual);
+            if (sinapsis != null) {
+                for (int i = 0; i < sinapsis.getTamaño(); i++) {
+                    Sinapsis s = sinapsis.obtener(i);
+                    int vecino = s.getDestino().getId();
+                    
+                    if (visitados.obtener(vecino) == null) {
+                        double peso = calcularPeso(s, diccionarioNeuro);
+                        double distanciaActual = distancias.obtener(nodoActual);
+                        double nuevaDistancia = distanciaActual + peso;
+                        double distanciaVecino = distancias.obtener(vecino);
+                        
+                        if (nuevaDistancia < distanciaVecino) {
+                            distancias.insertar(vecino, nuevaDistancia);
+                            anteriores.insertar(vecino, nodoActual);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return new ResultadoDijkstra(distancias, anteriores);
+    }
+    
+    /**
+     * Calcula el peso de una sinapsis usando la fórmula:
+     * peso = distancia / (velocidad * coeficienteEficiencia)
+     * 
+     * @param sinapsis Sinapsis a evaluar
+     * @param diccionarioNeuro Diccionario de neurotransmisores
+     * @return Peso calculado
+     */
+    private static double calcularPeso(Sinapsis sinapsis, TablaHash<String, Neurotransmisor> diccionarioNeuro) {
+        String idNeuro = sinapsis.getIdNeurotransmisor();
+        Neurotransmisor neuro = diccionarioNeuro.obtener(idNeuro);
+        
+        if (neuro == null) {
+            return sinapsis.getDistancia();
+        }
+        
+        double distancia = sinapsis.getDistancia();
+        double velocidad = neuro.getVelocidad();
+        double coeficiente = sinapsis.getCoeficienteEficiencia();
+        
+        if (velocidad == 0 || coeficiente == 0) {
+            return Double.MAX_VALUE;
+        }
+        
+        return distancia / (velocidad * coeficiente);
+    }
+    
+    /**
+     * Verifica si hay nodos no visitados.
+     */
+    private static boolean hayNoVisitados(TablaHash<Integer, Double> distancias, TablaHash<Integer, Boolean> visitados) {
+        Object[] claves = distancias.getClaves();
+        for (int i = 0; i < claves.length; i++) {
+            Integer id = (Integer) claves[i];
+            if (visitados.obtener(id) == null) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Obtiene el nodo no visitado con menor distancia.
+     */
+    private static int obtenerNodoNoVisitadoConMenorDistancia(TablaHash<Integer, Double> distancias, TablaHash<Integer, Boolean> visitados) {
+        int nodoMinimo = -1;
+        double distanciaMinima = Double.MAX_VALUE;
+        
+        Object[] claves = distancias.getClaves();
+        for (int i = 0; i < claves.length; i++) {
+            Integer id = (Integer) claves[i];
+            if (visitados.obtener(id) == null) {
+                double distancia = distancias.obtener(id);
+                if (distancia < distanciaMinima) {
+                    distanciaMinima = distancia;
+                    nodoMinimo = id;
+                }
+            }
+        }
+        
+        return nodoMinimo;
+    }
+    
+    /**
+     * Reconstruye el camino desde el origen hasta un destino.
+     * 
+     * @param resultado Resultado de Dijkstra
+     * @param idDestino ID de la neurona destino
+     * @return ListaEnlazada con el camino (incluye origen y destino)
+     */
+    public static ListaEnlazada<Integer> reconstruirCamino(ResultadoDijkstra resultado, int idDestino) {
+        ListaEnlazada<Integer> camino = new ListaEnlazada<>();
+        Integer actual = idDestino;
+        
+        while (actual != null) {
+            camino.agregarAlInicio(actual);
+            actual = resultado.anteriores.obtener(actual);
+        }
+        
+        return camino;
+    }
 }
--- src/proyecto_estructura_de_datos_sh_aq/Grafo.java
+++ src/proyecto_estructura_de_datos_sh_aq/Grafo.java
@@ -1,10 +1,77 @@
 package proyecto_estructura_de_datos_sh_aq;
 
 /**
- * Clase que implementa un Grafo Dirigido para la red neuronal.
- * 
+ * Grafo dirigido para la red neuronal.
  * @author santiagoh
  */
 public class Grafo {
     
+    private TablaHash<Integer, Neurona> neuronas;
+    private TablaHash<Integer, ListaEnlazada<Sinapsis>> adyacencia;
+    private TablaHash<String, Neurotransmisor> diccionarioNeuro;
+    
+    public Grafo() {
+        this.neuronas = new TablaHash<>();
+        this.adyacencia = new TablaHash<>();
+        this.diccionarioNeuro = new TablaHash<>();
+    }
+    
+    public void agregarNeurona(Neurona neurona) {
+        neuronas.insertar(neurona.getId(), neurona);
+        adyacencia.insertar(neurona.getId(), new ListaEnlazada<>());
+    }
+    
+    public void agregarSinapsis(Sinapsis sinapsis) {
+        int idOrigen = sinapsis.getOrigen().getId();
+        ListaEnlazada<Sinapsis> lista = adyacencia.obtener(idOrigen);
+        
+        if (lista != null) {
+            lista.agregar(sinapsis);
+        }
+    }
+    
+    public void agregarNeurotransmisor(Neurotransmisor neuro) {
+        diccionarioNeuro.insertar(neuro.getNombre(), neuro);
+    }
+    
+    public Neurona obtenerNeurona(int id) {
+        return neuronas.obtener(id);
+    }
+    
+    public ListaEnlazada<Sinapsis> obtenerVecinos(int idNeurona) {
+        return adyacencia.obtener(idNeurona);
+    }
+    
+    public Neurotransmisor obtenerNeurotransmisor(String nombre) {
+        return diccionarioNeuro.obtener(nombre);
+    }
+    
+    public TablaHash<Integer, Neurona> getNeuronas() {
+        return neuronas;
+    }
+    
+    public TablaHash<Integer, ListaEnlazada<Sinapsis>> getAdyacencia() {
+        return adyacencia;
+    }
+    
+    public TablaHash<String, Neurotransmisor> getDiccionarioNeuro() {
+        return diccionarioNeuro;
+    }
+    
+    public int getNumNeuronas() {
+        return neuronas.getTamaño();
+    }
+    
+    public int getNumSinapsis() {
+        int total = 0;
+        Object[] claves = adyacencia.getClaves();
+        for (int i = 0; i < claves.length; i++) {
+            Integer id = (Integer) claves[i];
+            ListaEnlazada<Sinapsis> lista = adyacencia.obtener(id);
+            if (lista != null) {
+                total += lista.getTamaño();
+            }
+        }
+        return total;
+    }
 }
